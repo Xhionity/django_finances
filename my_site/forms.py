192da642c1bf748fd792381ded9560ff314af49d
XhionX <Xhion@tut.by>
2022-12-09 19:29:30 +0300

    Correct adding new elements

--- my_site/forms.py
+++ my_site/forms.py
@@ -1,7 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Post
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Назначение'
+            }),
+            'content': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Сумма'
+            })
+        }
